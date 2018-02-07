35ee09b20ce6ea40aacf9d8af1c21e115fcaa569
Lewis Moore <lewis.moore2707@gmail.com>
2018-02-07 09:14:25 +0000

    slight change of routes text

--- app.js
+++ app.js
@@ -3,11 +3,11 @@ var app = express();
 var fighter = require('./routes/fighter.js')
 
 app.get('/', function(req, res) {
-  res.send('Fighting Homepage!');
+  res.send('Fight Express Homepage!');
 });
 
 app.get('/about', function(req, res) {
-  res.send('About Fighting Page!');
+  res.send('About Fight Express Page!');
 });
 
 app.listen(3000, function() {
