8c296f291c58c0c597898520746e1272a0347d3d
Lewis Moore <lewis.moore2707@gmail.com>
2018-02-13 22:22:23 +0000

    Player has no weapon by default, player can choose a weapon to use

--- spec/FighterSpec.js
+++ spec/FighterSpec.js
@@ -11,4 +11,13 @@ describe('Fighter', function(){
   it('A players attack power starts at 10', function(){
     expect(fighter.stats()).toEqual(10)
   })
+
+  it('A player is unarmed by default', function(){
+    expect(fighter.showWeapon()).toEqual('unarmed')
+  })
+
+  it('A player can choose a weapon for their fighter', function(){
+    fighter.chooseWeapon('axe');
+    expect(fighter.showWeapon()).toEqual('axe')
+  })
 })
