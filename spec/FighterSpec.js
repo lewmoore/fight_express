fb2d9ba19e16302f3c4b213f39a6192dace7f97b
Lewis Moore <lewis.moore2707@gmail.com>
2018-02-13 22:07:33 +0000

    Implement attack power and default to 10

--- spec/FighterSpec.js
+++ spec/FighterSpec.js
@@ -7,4 +7,8 @@ describe('Fighter', function(){
   it('A fighter starts with 100 health', function(){
     expect(fighter.health()).toEqual(100);
   })
+
+  it('A players attack power starts at 10', function(){
+    expect(fighter.stats()).toEqual(10)
+  })
 })
